b2bf6f453bc1d3c65c412ba030a58ab71e000827
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-11-26 02:07:56 +0500

    Another commit

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -8,42 +8,43 @@ const roboto = Roboto({
 
 const Hero = () => {
   return (
-    <div>
-      <div className="flex justify-between">
-        <div className=" w-[640px] h-[300px] mt-64 ml-20 ]">
-          <h1
-            className={`${roboto.className} font-bold text-[56px] w-[500px] h-[134px]`}
+    <div className="flex flex-col lg:flex-row justify-between">
+      {/* Left Section */}
+      <div className="w-full lg:w-[640px] mt-16 mx-4 lg:mt-64 lg:ml-20">
+        <h1
+          className={`${roboto.className} font-bold text-[32px] sm:text-[48px] lg:text-[56px] w-full`}
+        >
+          Learn new skills online with ease
+        </h1>
+        <p
+          className={`${roboto.className} mt-6 sm:text-[16px] lg:text-[18px] w-full lg:w-[500px]`}
+        >
+          Discover a wide range of courses covering a variety of subjects,
+          taught by expert instructors.
+        </p>
+        <div className="flex flex-col sm:flex-row mt-7">
+          <button
+            className={`${roboto.className} text-[16px] font-normal w-auto h-auto rounded-[5px] border px-[24px] py-[12px] text-white bg-black mb-4 sm:mb-0 sm:mr-4`}
           >
-            Learn new skills online with ease
-          </h1>
-          <p
-            className={`${roboto.className}  mt-14 font-normal text-[18px] w-[500px] h-[54px]`}
+            Start learning now
+          </button>
+          <button
+            className={`${roboto.className} text-[16px] font-normal w-auto h-auto rounded-[5px] border px-[24px] py-[12px] border-black`}
           >
-            Discover a wide range of courses covering a variety of subjects,
-            taught by expert instructors.
-          </p>
-          <div>
-            <button
-              className={`${roboto.className} text-[16px] font-normal w-auto h-auto rounded-[5px] border px-[24px] py-[12px] text-white bg-black mt-7 `}
-            >
-              Start learning now
-            </button>
-            <button
-              className={`${roboto.className} text-[16px] font-normal w-auto h-auto rounded-[5px] border px-[24px] py-[12px] border-black ml-4`}
-            >
-              Explore Courses
-            </button>
-          </div>
+            Explore Courses
+          </button>
         </div>
-        <div className=" h-[900]">
-          {" "}
-          <Image
-            src="/Image/heroimage.jpg"
-            alt="Hero Image"
-            width={640}
-            height={900}
-          ></Image>
-        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-full lg:w-[640px] h-auto mt-8 lg:mt-0">
+        <Image
+          src="/Image/heroimage.jpg"
+          alt="Hero Image"
+          width={640}
+          height={900}
+          className="w-full h-auto"
+        />
       </div>
     </div>
   );
